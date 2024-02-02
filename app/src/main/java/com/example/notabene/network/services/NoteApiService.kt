4124518c5bc495618ce7e655ef4903b94a6b8369
Mickaël <60145073+MickaelMoon@users.noreply.github.com>
2024-02-02 11:09:39 +0100

    deleting import

--- app/src/main/java/com/example/notabene/network/services/NoteApiService.kt
+++ app/src/main/java/com/example/notabene/network/services/NoteApiService.kt
@@ -2,8 +2,7 @@ package com.example.notabene.network.services
 
 import com.example.notabene.model.note_model.NoteData
 import com.example.notabene.model.note_model.NoteDto
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Path
