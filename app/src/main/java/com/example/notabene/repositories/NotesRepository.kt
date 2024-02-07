78a496033bd35d656b8d9c699836204c741f4899
NicolasMontes0 <montes.nicolas18@gmail.com>
2024-02-07 22:50:19 +0100

    add getDeletedNotesByUserId(userId) to get data

--- app/src/main/java/com/example/notabene/repositories/NotesRepository.kt
+++ app/src/main/java/com/example/notabene/repositories/NotesRepository.kt
@@ -2,7 +2,6 @@ package com.example.notabene.repositories
 
 import com.example.notabene.model.note_model.ChangeNoteResponse
 import com.example.notabene.model.note_model.NoteData
-import com.example.notabene.model.note_model.createNoteBody
 import com.example.notabene.network.services.NoteApiService
 import io.reactivex.rxjava3.core.Flowable
 
@@ -21,5 +20,13 @@ class NotesRepository(
 
     suspend fun deleteNote(noteId: Int): ChangeNoteResponse {
         return this.noteService.deleteNote(noteId)
+    }
+
+    fun getDeletedNotesByUserId(userId: String): Flowable<List<NoteData>> {
+        return noteService.getDeletedNotesByUserId(
+            userId,
+        ).map {
+            it
+        }
     }
 }
--- app/src/main/java/com/example/notabene/viewmodel/NotesViewModel.kt
+++ app/src/main/java/com/example/notabene/viewmodel/NotesViewModel.kt
@@ -6,13 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.notabene.model.note_model.ChangeNoteResponse
 import com.example.notabene.model.note_model.NoteData
-import com.example.notabene.model.note_model.createNoteBody
 import com.example.notabene.repositories.NotesRepository
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.subjects.BehaviorSubject
-import java.text.SimpleDateFormat
-import java.util.Date
 
 
 class NotesViewModel(
@@ -55,5 +52,28 @@ class NotesViewModel(
 
     suspend fun deleteNote(noteId: Int): ChangeNoteResponse {
         return this.notesRepo.deleteNote(noteId)
+    }
+
+    fun getDeletedNotesByUserId(userId: String) {
+        this.notesRepo.getDeletedNotesByUserId(userId).subscribe({ notes ->
+            this.notesData.onNext(notes)
+            Log.d("Deleted notes", notes.toString())
+            this.completeNotesList.postValue(List<NoteData>(notes.size) {
+                NoteData(
+                    notes[it].noteId,
+                    notes[it].title,
+                    notes[it].description,
+                    notes[it].userId,
+                    notes[it].flagsup,
+                    notes[it].date,
+                    notes[it].categoryId,
+                    notes[it].category
+                )
+            })
+        }, { error ->
+            Log.d("Error in function getDeletedNotesByUserId",
+                error.message ?: "Default message error"
+            )
+        }).addTo(disposeBag)
     }
 }
